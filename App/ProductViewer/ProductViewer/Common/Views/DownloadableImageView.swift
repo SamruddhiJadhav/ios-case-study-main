ac59188e3d1c0023117c76b1f605a89a84961da2
Samruddhi Jadhav <ani.sam2015@gmail.com>
2024-11-28 12:05:31 +0800

    Improvements: Added new DownloadableImageView, which can download a image

--- App/ProductViewer/ProductViewer/Common/Views/DownloadableImageView.swift
+++ App/ProductViewer/ProductViewer/Common/Views/DownloadableImageView.swift
@@ -6,4 +6,49 @@
 //  Copyright © 2024 Target. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import SDWebImage
+
+final class DownloadableImageView: UIImageView {
+    private let imageProvider: ImageProviderProtocol
+
+    init(_ imageProvider: ImageProviderProtocol = ImageProvider()) {
+        self.imageProvider = imageProvider
+        super.init(frame: .zero)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func image(
+      for imageUrl: String,
+      contentMode: ContentMode? = nil,
+      placeholder: UIImage? = UIImage(named: "placeholder"),
+      size: CGSize,
+      completion: (() -> Void)? = nil
+    ) {
+        clipsToBounds = true
+        self.contentMode = .center
+        placeholderFallback(placeholder)
+
+        if let downloadableImageUrl = URL(string: imageUrl) {
+            imageProvider.loadImage(from: downloadableImageUrl, resizeTo: size) { [weak self] image in
+              DispatchQueue.main.async {
+                  self?.image = image
+              }
+            }
+        }
+     }
+      
+      func cancelDownload() {
+          imageProvider.cancelCurrentImageLoad()
+      }
+}
+
+private extension DownloadableImageView {
+    func placeholderFallback(_ placeholder: UIImage?) {
+        contentMode = .center
+        image = placeholder
+    }
+}
